0582f70344fec44da42f502d080522a88822122c
Madeo Arturi <madeo.arturi@gmail.com>
2023-10-01 00:33:12 -0400

    Weapon system complete

--- LD54/Assets/Scripts/Abilities/Ability.cs
+++ LD54/Assets/Scripts/Abilities/Ability.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public abstract class Ability : MonoBehaviour
 {
-    public float cooldown = 2f;
-    private float currentCooldown = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +13,7 @@ public abstract class Ability : MonoBehaviour
     // Update is called once per frame
     protected void Update()
     {
-        currentCooldown += Time.deltaTime;
-        if (Input.GetKeyDown(KeyCode.Mouse0) && currentCooldown >= cooldown)
-        {
-            execute();
-            currentCooldown = 0;
-        }
+
     }
 
     // Overiddable method to use an ability
